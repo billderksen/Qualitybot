27d3c1a545fc523269855b5addd41fca5d90c573
Youri de Vlam <youridevlam@outlook.com>
2021-10-15 21:11:19 +0200

    Create delsfx.js

---
+++ commands/delsfx.js
@@ -0,0 +1,22 @@
+const soundfxModel = require('../models/soundFx')
+
+module.exports = {
+    name: 'delsfx',
+    description: 'Delete sound effect from the mongodb',
+
+    async execute(client, message, args) {
+        try {
+            const query = await soundfxModel.findOne({ name: args[0], server: message.guild.id});
+
+            if (query) {
+                const sfx = await soundfxModel.deleteOne(query)
+                message.channel.send(`**Deleted SFX:** ${query.name}`)
+            } else {
+                message.channel.send(`Your guild does not have this soundFX!`)
+            }
+        } catch (err) {
+            message.channel.send(`Error code: ${err.code}`)
+            throw err;
+        }
+    }
+}
