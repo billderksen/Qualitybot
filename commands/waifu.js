13a3a1550be947d1ca251758a60ee02a76219ac1
Youri de Vlam <youridevlam@outlook.com>
2021-10-30 01:36:07 +0200

    Update waifu.js

--- commands/waifu.js
+++ commands/waifu.js
@@ -4,16 +4,21 @@ require('dotenv').config()
 
 module.exports = {
     name: 'waifu',
-    description: `Get an image from ${process.env.BOORUSITE}`,
+    description: `Get an image from ${process.env.BOORUSITE}. Example: "Shirakami Fubuki" "Hololive"`,
 
     async execute(client, message, args, Discord) {
 
-        Booru.search(`${process.env.BOORUSITE}`, [`${args.join('_')}`], { limit: 1, random: true })
+        let query = [];
+        for (let t of args.join(" ").split("\"").filter(e => e.length > 1)) {
+            query.push(t.replace(/\s+/g,"_"))
+        }
+
+        Booru.forSite(process.env.BOORUSITE).search(query, { limit: 1, random: true })
         .then(posts => {
             if (posts.length > 0) {
                 return message.channel.send(posts[0].fileUrl)
             } else {
-                return message.channel.send(`No results for ${args.join('_')}`)
+                return message.channel.send(`No results for ${args.join(' ')}`)
             }
 
         })
