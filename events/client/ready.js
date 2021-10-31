8d172c1b41a9bd95f7ab64a7bda0fc781f61aca3
Youri <youridevlam@outlook.com>
2021-10-31 12:46:25 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,14 @@
-module.exports = () => {
+require('dotenv').config()
+module.exports = (Discord, client) => {
     console.log('Qualitybot is online')
+    //client.user.setActivity("Game");
+    client.user.setStatus('available')
+    client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: ` ${process.env.PREFIX}commands for help`,
+            type: 'STREAMING',
+            url: 'https://www.twitch.tv/qualitytime'
+        }
+    });
 }
