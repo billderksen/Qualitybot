b644eb39e22edd6ccd1e23ae5022e6232d9c40a3
Youri de Vlam <youridevlam@outlook.com>
2021-11-01 19:28:55 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,33 +1,33 @@
 module.exports = {
     name: 'stats',
-    description: 'Lists all commands',
+    description: 'Lists all stats',
     pings: 0,
     async execute(client, message, args, Discord) {
 
-        let commands = [];
+        let stats = [];
         let i = 0;
         for (const [key, value] of args.entries()) {
-            commands.push({name: '', value: ''});
-            commands[i].name = `__${value.name}__`;
-            commands[i].value = `Calls: ${value.pings}`;
-            commands[i].inline = true;
+            stats.push({name: '', value: ''});
+            stats[i].name = `__${value.name}__`;
+            stats[i].value = `Calls: ${value.pings}`;
+            stats[i].inline = true;
             i++;
         }
 
-        const commandsEmbed = new Discord.MessageEmbed()
+        const statsEmbed = new Discord.MessageEmbed()
         .setColor('#ff0000')
-        .setTitle('Qualitybot Commands')
+        .setTitle('Qualitybot Statistics')
         .setURL('https://github.com/yourimv/Qualitybot')
         .setAuthor('Youri | Qualitytime', 'https://i.imgur.com/A2lw57a.png', 'https://github.com/yourimv/Qualitybot')
         .setThumbnail('https://i.imgur.com/A2lw57a.png')
         .addFields(
             { name: 'Uptime', value: `${new Date((client.uptime / 1000) * 1000).toISOString().substr(11, 8)}` },
-            commands
+            stats
         )
         // .setImage('https://i.imgur.com/AfFp7pu.png')
         .setTimestamp()
         .setFooter('Click the link to view the bot\'s source code', 'https://i.imgur.com/YzyMfFO.png');
 
-        message.channel.send(commandsEmbed);
+        message.channel.send(statsEmbed);
     }
 }
