367dd5c1ac07bc5d47a14e2a01d265eaccb0da8e
Youri de Vlam <youridevlam@outlook.com>
2021-12-04 03:50:14 +0100

    cluster for reboot on unhandled exception

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
+var cluster = require('cluster');
 const Discord = require('discord.js')
 require('dotenv').config()
 const client = new Discord.Client()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_SRV, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
- }).then(() => {
-     console.log('Connected to mongoDB')
- }).catch((err) => {
-     console.log(err)
- })
+if (cluster.isMaster) {
+    cluster.fork();
 
-client.commands = new Discord.Collection()
-client.events = new Discord.Collection()
+    cluster.on('exit', function (worker, code, signal) {
+        cluster.fork();
+    });
+}
 
-const handlers = ['command_handler', 'event_handler']
-handlers.forEach(handler => {
-    require(`./handlers/${handler}`)(client, Discord)
-})
+if (cluster.isWorker) {
+    console.log('starting')
+    mongoose.connect(process.env.MONGODB_SRV, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log('Connected to mongoDB')
+    }).catch((err) => {
+        console.log(err)
+    })
 
-client.login(process.env.DISCORD_TOKEN)
+    client.commands = new Discord.Collection()
+    client.events = new Discord.Collection()
+
+    const handlers = ['command_handler', 'event_handler']
+    handlers.forEach(handler => {
+        require(`./handlers/${handler}`)(client, Discord)
+    })
+
+    client.login(process.env.DISCORD_TOKEN)
+}
+
